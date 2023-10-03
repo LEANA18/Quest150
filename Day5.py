061e57e3bbce0dd62572942fa0d72eea49c14e8c
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-10-03 21:53:32 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,26 @@
+Majority Element
+*****************
+Q)Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+
+Program
+*******
+  class Solution(object):
+    def majorityElement(self, nums):
+        n=len(nums)
+        nums.sort()
+        return nums[n//2]
+
+
+Example 1:
+**********
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+**********
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
